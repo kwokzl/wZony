55300170ca843167e0d27fe4e6aa9292ec818b69
Kwok <31276761+KwokZL@users.noreply.github.com>
2025-06-01 10:51:15 +0800

    Add default css

--- PythonWebUI/HTMLBulider/builder.py
+++ PythonWebUI/HTMLBulider/builder.py
@@ -1,5 +1,10 @@
 from ..core import *
 from .. import HTMLNodes as nodes
+import os
+ 
+current_path = os.path.abspath(__file__)
+package_dir = os.path.dirname(current_path)
+coreCss_path = os.path.join(package_dir, 'core.css')
 
 # def html(self, innerText:str="", innerHTML:HTMLSet=None, lang:str="", **attribute)->HTMLElement:
 #     return nodes.Html(innerText, innerHTML, lang, attribute)
@@ -27,3 +32,13 @@ def NavigationBar(title:str,navigationNodes:list[HTMLElement]):
         ]),class_="cont")
     ]))
     
+def Page(body:HTMLSet,head:HTMLSet=None):
+    with open(coreCss_path) as coreCss:
+        return nodes.Html(HTMLSet([
+            nodes.Head(HTMLSet([
+                head if head else HTMLSet(),
+                nodes.Meta(charset='utf-8'),
+                nodes.Style(coreCss.read())
+            ])),
+            nodes.Body(body)
+        ]))
