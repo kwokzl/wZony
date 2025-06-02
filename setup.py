4cb53a46f8c82ab53786ad7ca7e5b6c048b2926a
Kwok <31276761+KwokZL@users.noreply.github.com>
2025-06-02 19:57:12 +0800

    Update for PyPI

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("Readme.md", "r", encoding="utf-8") as fh:
+with open("./docs/PyPI.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -10,7 +10,18 @@ setuptools.setup(
     description="Rapid HTML Construction with Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kwokzl/PythonWebUI",
+    #url="https://github.com/kwokzl/PythonWebUI",
     packages=setuptools.find_packages(),
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    project_urls={
+        
+        "Source": "https://github.com/kwokzl/PythonWebUI",
+        "Tracker": "https://github.com/kwokzl/PythonWebUI/issues",
+    },
+    classifiers=[
+        "License :: OSI APPROVED :: APACHE SOFTWARE LICENSE",
+        "TOPIC :: INTERNET :: WWW/HTTP",
+        "TOPIC :: TEXT PROCESSING :: MARKUP :: HTML",
+        "PROGRAMMING LANGUAGE :: PYTHON"
+    ]
 )
