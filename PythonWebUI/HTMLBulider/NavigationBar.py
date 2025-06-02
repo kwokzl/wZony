38d86b32bdfd3a4f5aea2733fee52a9e1d277a21
Kwok <31276761+KwokZL@users.noreply.github.com>
2025-06-02 23:46:17 +0800

    Revert "refactor: Return css based on usage"
    This reverts commit 3e48be79eef8c3edd0198cd7bdfbf15e66c882ee.

--- PythonWebUI/HTMLBulider/NavigationBar.py
+++ PythonWebUI/HTMLBulider/NavigationBar.py
@@ -1,36 +1,8 @@
 from ..core import *
 from .. import HTMLNodes as nodes
 
-specificStyle="""
-nav{
-    border-top: rgb(218, 218, 218) 1px solid;
-    border-bottom: rgb(218, 218, 218) 1px solid;
-    box-shadow: 0px 0px 20px rgba(218, 218, 218,0.6);
-}
-
-nav *{
-    display:inline-block;
-    align-items:center;
-}
-nav .cont{
-    display:flex;
-    justify-content: space-around;
-    font-size:20px;
-    
-
-}
-
-nav div{
-    padding:0;
-}
-
-nav .title{
-    font-size:35px;
-}
-"""
-
 def NavigationBar(title:str,navigationNodes:list[HTMLElement]):
-    element=nodes.Nav(HTMLSet([
+    return nodes.Nav(HTMLSet([
         nodes.Div(HTMLSet([
             nodes.Div(title,class_="title"),
             nodes.Ul(
@@ -39,5 +11,4 @@ def NavigationBar(title:str,navigationNodes:list[HTMLElement]):
             )
         ]),class_="cont")
     ]))
-    element.specificStyle=specificStyle
-    return element
+
