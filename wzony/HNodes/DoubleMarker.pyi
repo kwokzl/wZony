8f74f0ee1433e59fcee01d01887ceeb90cf0e26a
Kwok <31276761+KwokZL@users.noreply.github.com>
2025-06-12 17:41:15 +0800

    refactor: add pyi files

---
+++ wzony/HNodes/DoubleMarker.pyi
@@ -0,0 +1,182 @@
+
+from ..core import *
+
+class Html(DoubleMarker):
+    def __init__(self,lang:str):...
+
+
+class Head(DoubleMarker):
+    def __init__(self,profile:str):...
+
+
+class Body(DoubleMarker):
+    def __init__(self,bgcolor:str,text:str,link:str,vlink:str,alink:str):...
+
+
+class Title(DoubleMarker):
+    def __init__(self,):...
+
+
+class Script(DoubleMarker):
+    def __init__(self,src:str,type:str,charset:str,async_:bool,defer:bool):...
+
+
+class Style(DoubleMarker):
+    def __init__(self,type:str,media:str):...
+
+
+class Noscript(DoubleMarker):
+    def __init__(self,):...
+
+
+class Template(DoubleMarker):
+    def __init__(self,):...
+
+
+class Div(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class Span(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class P(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class H1(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class H2(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class H3(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class H4(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class H5(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class H6(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class A(DoubleMarker):
+    def __init__(self,href:str,target:str,rel:str,download:str):...
+
+
+class Strong(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class B(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class I(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class EM(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class U(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class Sup(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class Sub(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class Table(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str,border:str,cellpadding:str,cellspacing:str):...
+
+
+class Thead(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class Tbody(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class Tr(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class Td(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str,colspan:str,rowspan:str):...
+
+
+class Th(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str,colspan:str,rowspan:str):...
+
+
+class form(DoubleMarker):
+    def __init__(self,action:str,method:str,id:str,class_:str,style:str,enctype:str):...
+
+
+class Input(DoubleMarker):
+    def __init__(self,type:str,name:str,value:str,placeholder:str,id:str,class_:str,style:str,disabled:bool,readonly:bool,required:bool):...
+
+
+class Button(DoubleMarker):
+    def __init__(self,type:str,id:str,class_:str,style:str,disabled:bool):...
+
+
+class Label(DoubleMarker):
+    def __init__(self,for_:str,id:str,class_:str,style:str):...
+
+
+class Textarea(DoubleMarker):
+    def __init__(self,rows:str,cols:str,id:str,class_:str,style:str,disabled:bool):...
+
+
+class Ul(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class Ol(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str,start:str,type:str):...
+
+
+class Li(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class Article(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class Aside(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class Footer(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class Header(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class Nav(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
+
+class Section(DoubleMarker):
+    def __init__(self,id:str,class_:str,style:str):...
+
